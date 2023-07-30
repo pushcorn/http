81d77d016f891a7e00657b698687ba1c09e021a0
Joseph Tzeng <joseph@pushcorn.com>
2023-07-29 21:19:51 -0400

    returns an empty string from the server command in CLI mode

--- test/tests/lib/http/commands/Server.js
+++ test/tests/lib/http/commands/Server.js
@@ -1,21 +1,38 @@
 test.command ("http.commands.Server")
     .should ("create and start an instance of http.Server")
-    .mock (nit, "log")
-    .given (
-    {
-        port: 0,
-        stopTimeout: 0
-    })
-    .after (async (s) =>
-    {
-        await nit.sleep (20);
+        .mock (nit, "log")
+        .given (
+        {
+            port: 0,
+            stopTimeout: 0
+        })
+        .after (async (s) =>
+        {
+            await nit.sleep (20);
 
-        s.port = s.result.realPort;
+            s.port = s.result.realPort;
 
-        await s.result.stop ();
-        await nit.sleep (20);
-    })
-    .expectingPropertyToBe ("mocks.0.invocations.0.args.0", /server started/)
-    .expectingPropertyToBe ("port", /\d+/)
-    .commit ()
+            await s.result.stop ();
+            await nit.sleep (20);
+        })
+        .expectingPropertyToBe ("mocks.0.invocations.0.args.0", /server started/)
+        .expectingPropertyToBe ("port", /\d+/)
+        .commit ()
+
+    .should ("not return the server if meta property returnServer is false")
+        .mock (nit, "log")
+        .before (s => s.class.returnServer = false)
+        .given (
+        {
+            port: 0,
+            stopTimeout: 0
+        })
+        .after (async (s) =>
+        {
+            await nit.sleep (20);
+            await s.object.server.stop ();
+            await nit.sleep (20);
+        })
+        .returns ("")
+        .commit ()
 ;
