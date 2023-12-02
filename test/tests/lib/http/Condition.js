9c6a36e788a5460dcf7897de0aed5c353cf6f53c
Joseph Tzeng <joseph@pushcorn.com>
2023-12-02 14:09:29 -0500

    allows Condition to be used as an instance plugin

--- test/tests/lib/http/Condition.js
+++ test/tests/lib/http/Condition.js
@@ -1,48 +1,30 @@
-test.method ("http.Condition", "onRegisterPlugin", true)
-    .should ("add the applicableTo method to the host class")
-        .given (nit.defineClass ("Filter", true))
-        .after (function ()
-        {
-            const TestCond = nit.defineClass ("TestCond", "http.Condition")
-                .method ("check", (ctx, owner) =>
-                {
-                    this.checkCtx = ctx;
-                    this.checkOwner = owner;
-
-                    return true;
-                })
-            ;
-
-            const Filter = this.args[0];
-
-            Filter.registerPlugin (this.class);
-            Filter.condition (new TestCond);
-
-            this.filter = new Filter;
-            this.isFilterApplicable = this.filter.applicableTo (nit.require ("http.Context").new ("GET", "/users"));
-        })
-        .expectingPropertyToBeOfType ("args.0.prototype.applicableTo", "function")
-        .expectingPropertyToBe ("isFilterApplicable", true)
-        .expectingPropertyToBeOfType ("checkCtx", "http.Context")
-        .expectingPropertyToBeOfType ("checkOwner", "Filter")
+test.plugin ("http.Condition", "applicableTo", { registerPlugin: true })
+    .should ("return true if the every condition passed the check")
+        .before (s => s.TrueCondition = s.pluginClass.defineSubclass ("test.conditions.TrueCondition")
+            .onCheck (() => s.checked = true)
+        )
+        .before (s => s.hostClass.condition ("test:true-condition"))
+        .returns (true)
+        .expectingPropertyToBe ("checked", true)
         .commit ()
 
-    .should ("only check the first condition of every type")
-        .given (nit.defineClass ("MyApi", "http.Api"))
-        .after (s =>
-        {
-            const MyApi = s.args[0];
+    .should ("return false if the some condition failed")
+        .before (s => s.FalseCondition = s.pluginClass.defineSubclass ("test.conditions.FalseCondition")
+            .onCheck (() => s.checked = false)
+        )
+        .before (s => s.hostClass.condition ("test:false-condition"))
+        .returns (false)
+        .expectingPropertyToBe ("checked", false)
+        .commit ()
 
-            MyApi.condition ("http:request-path", "/api1");
-
-            const ChildApi = MyApi.defineSubclass ("ChildApi");
-
-            ChildApi.condition ("http:request-path", "/child-api");
-
-            s.applicableToChildApiPath = ChildApi ().applicableTo (s.Context.new ("GET", "/child-api"));
-            s.applicableToMyApiPath = ChildApi ().applicableTo (s.Context.new ("GET", "/api1"));
-        })
-        .expectingPropertyToBe ("applicableToChildApiPath", true)
-        .expectingPropertyToBe ("applicableToMyApiPath", false)
+    .should ("check the instance conditions")
+        .init (s => s.instancePluginAllowed = true)
+        .before (s => s.FalseCondition = s.pluginClass.defineSubclass ("test.conditions.InstCondition")
+            .onCheck (() => s.checked = true)
+        )
+        .before (s => s.hostClass.condition.call (s.host, "test:inst-condition"))
+        .returns (true)
+        .expectingPropertyToBe ("checked", true)
+        .expectingPropertyToBe ("host.conditions.length", 1)
         .commit ()
 ;
