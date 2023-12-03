d6cad1fa2276ab2495bbd1428c4f6b7c1c8d560e
Joseph Tzeng <joseph@pushcorn.com>
2023-12-03 14:59:16 -0500

    makes http.AssetResolver a plugin

--- test/tests/lib/http/AssetResolver.js
+++ test/tests/lib/http/AssetResolver.js
@@ -1,32 +1,18 @@
-test.method ("http.AssetResolver.Descriptor", "build")
-    .should ("return an AssetResolver")
-        .up (s => s.createArgs =
-        {
-            extensions: ".png",
-            roots: nit.path.join (test.TEST_PROJECT_PATH, "resources")
-        })
-        .returnsInstanceOf ("http.AssetResolver")
-        .expectingPropertyToBe ("result.extensions", [".png"])
-        .expectingPropertyToBe ("result.roots", [nit.path.join (test.TEST_PROJECT_PATH, "resources")])
-        .commit ()
-;
-
-
-test.method ("http.AssetResolver", "resolve")
+test.plugin ("http.AssetResolver", "resolveAsset", { instancePluginAllowed: true, registerPlugin: true, addPlugin: "instance" })
     .should ("return the absolute path of a file under the specified root directories")
-        .up (s => s.createArgs = { roots: nit.path.join (test.TEST_PROJECT_PATH, "resources") })
+        .init (s => s.pluginArgs = { roots: nit.path.join (test.TEST_PROJECT_PATH, "resources") })
         .given ("html/page-one.html")
         .returns (nit.path.join (test.TEST_PROJECT_PATH, "resources/html/page-one.html"))
         .commit ()
 
     .should ("return undefined if the file cannot be found")
-        .up (s => s.createArgs = { roots: nit.path.join (test.TEST_PROJECT_PATH, "resources") })
+        .init (s => s.pluginArgs = { roots: nit.path.join (test.TEST_PROJECT_PATH, "resources") })
         .given ("html/page-three.html")
         .returns ()
         .commit ()
 
     .should ("return undefined if the file does not match the specifed extension")
-        .up (s => s.createArgs =
+        .init (s => s.pluginArgs =
         {
             extensions: ".png",
             roots: nit.path.join (test.TEST_PROJECT_PATH, "resources")
