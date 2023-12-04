cfc9e4d2aaf85babd24320d92aa013f8536ce069
Joseph Tzeng <joseph@pushcorn.com>
2023-12-04 13:37:30 -0500

    updates Host to use lifecycle-component

--- test/tests/lib/http/Conditional.js
+++
@@ -1,9 +0,0 @@
-const Conditional = nit.require ("http.Conditional");
-
-
-test.object (Conditional)
-    .should ("be a class that uses the condition plugin")
-    .expectingPropertyToBeOfType ("class.condition", "function")
-    .expectingPropertyToBeOfType ("class.conditions", Array)
-    .commit ()
-;
--- test/tests/lib/http/Host.js
+++ test/tests/lib/http/Host.js
@@ -1,160 +1,195 @@
-test.method ("http.Host.Descriptor", "build")
-    .should ("build a host object")
-        .up (s =>
+test.object ("http.Host")
+    .should ("have a hostname getter")
+        .given (
         {
-            s.createArgs =
+            hostnames: "app.pushcorn.com",
+            services: "http:file-server",
+            requestFilters: "http:json-body-parser",
+            responseFilters: "http:body-compressor",
+            certificate:
             {
-                names: "app.pushcorn.com",
-                services: "http:file-server",
-                requestFilters: "http:json-body-parser",
-                responseFilters: "http:body-compressor",
-                certificate:
-                {
-                    cert: "pushcorn.com.crt",
-                    key: "pushcorn.com.key",
-                    ca: "ca.pushcorn.com.crt"
-                }
-            };
+                cert: "pushcorn.com.crt",
+                key: "pushcorn.com.key",
+                ca: "ca.pushcorn.com.crt"
+            }
         })
         .returnsInstanceOf ("http.Host")
-        .expectingPropertyToBe ("result.constructor.conditions.length", 1)
-        .expectingPropertyToBe ("result.names", ["app.pushcorn.com"])
+        .expectingPropertyToBe ("result.conditions.length", 1)
+        .expectingPropertyToBe ("result.hostnames", ["app.pushcorn.com"])
         .expectingPropertyToBeOfType ("result.certificate", "http.Certificate")
         .expectingPropertyToBeOfType ("result.requestFilters.0", "http.requestfilters.JsonBodyParser")
         .expectingPropertyToBeOfType ("result.responseFilters.0", "http.responsefilters.BodyCompressor")
         .expectingPropertyToBeOfType ("result.services.0", "http.services.FileServer")
-        .expectingPropertyToBe ("result.name", "app.pushcorn.com")
+        .expectingPropertyToBe ("result.hostname", "app.pushcorn.com")
         .commit ()
 
     .reset ()
-        .up (s =>
+        .given (
         {
-            s.createArgs =
+            services: "http:file-server",
+            requestFilters: "http:json-body-parser",
+            responseFilters: "http:body-compressor",
+            certificate:
             {
-                services: "http:file-server",
-                requestFilters: "http:json-body-parser",
-                responseFilters: "http:body-compressor",
-                certificate:
-                {
-                    cert: "pushcorn.com.crt",
-                    key: "pushcorn.com.key",
-                    ca: "ca.pushcorn.com.crt"
-                }
-            };
+                cert: "pushcorn.com.crt",
+                key: "pushcorn.com.key",
+                ca: "ca.pushcorn.com.crt"
+            }
         })
         .returnsInstanceOf ("http.Host")
-        .expectingPropertyToBe ("result.constructor.conditions.length", 0)
-        .expectingPropertyToBe ("result.name", "")
+        .expectingPropertyToBe ("result.conditions.length", 0)
+        .expectingPropertyToBe ("result.hostname", "")
         .commit ()
 ;
 
 
 test.method ("http.Host", "init")
     .should ("initialize the services")
-    .before (s =>
+    .up (s =>
     {
+        s.called = [];
+
         const Service = s.http.defineService ("TestService")
-            .onInit (function ()
-            {
-                this.initArgs = nit.array (arguments);
-                this.initCalled = true;
-            })
+            .onPreInit (() => s.called.push ("preInitService"))
+            .onPostInit (() => s.called.push ("postInitService"))
+            .onInit (() => s.called.push ("initService"))
         ;
 
-        s.object.services.push (new Service ());
-    })
-    .given (nit.new ("http.Server"))
-    .returns ()
-    .expectingPropertyToBe ("object.services.0.initCalled", true)
-    .expectingPropertyToBeOfType ("object.services.0.initArgs.0", "http.Server")
-    .commit ()
-;
-
-
-test.method ("http.Host", "upgrade")
-    .should ("invoke services' upgrade method")
-    .before (s =>
-    {
-        const Service = s.http.defineService ("TestService")
-            .onUpgrade (function ()
-            {
-                this.upgradeArgs = nit.array (arguments);
-                this.upgradeCalled = true;
-            })
+        s.class = s.class.defineSubclass ("MyHost")
+            .onPreInit (() => s.called.push ("preInitHost"))
+            .onPostInit (() => s.called.push ("postInitHost"))
+            .onInit (() => s.called.push ("initHost"))
         ;
 
-        s.object.services.push (new Service ());
+        s.createArgs = { services: new Service () };
     })
-    .given (1, 2, 3)
-    .returns ()
-    .expectingPropertyToBe ("object.services.0.upgradeCalled", true)
-    .expectingPropertyToBe ("object.services.0.upgradeArgs", [1, 2, 3])
+    .before (s => s.object.preInit ())
+    .after (s => s.object.postInit ())
+    .returnsResultOfExpr ("object")
+    .expectingPropertyToBe ("called", ["preInitHost", "preInitService", "initHost", "initService", "postInitHost", "postInitService"])
     .commit ()
 ;
 
 
 test.method ("http.Host", "start")
     .should ("invoke services' start method")
-    .before (s =>
+    .up (s =>
     {
+        s.called = [];
+
         const Service = s.http.defineService ("TestService")
-            .onStart (function ()
-            {
-                this.startArgs = nit.array (arguments);
-                this.startCalled = true;
-            })
+            .onPreStart (() => s.called.push ("preStartService"))
+            .onPostStart (() => s.called.push ("postStartService"))
+            .onStart (() => s.called.push ("startService"))
         ;
 
-        s.object.services.push (new Service ());
+        s.class = s.class.defineSubclass ("MyHost")
+            .onPreStart (() => s.called.push ("preStartHost"))
+            .onPostStart (() => s.called.push ("postStartHost"))
+            .onStart (() => s.called.push ("startHost"))
+        ;
+
+        s.createArgs = { services: new Service () };
     })
-    .given (1, 2, 3)
-    .returns ()
-    .expectingPropertyToBe ("object.services.0.startCalled", true)
-    .expectingPropertyToBe ("object.services.0.startArgs", [])
+    .before (s => s.object.preStart ())
+    .after (s => s.object.postStart ())
+    .returnsResultOfExpr ("object")
+    .expectingPropertyToBe ("called", ["preStartHost", "preStartService", "startHost", "startService", "postStartHost", "postStartService"])
     .commit ()
 ;
 
 
 test.method ("http.Host", "stop")
     .should ("invoke services' stop method")
-    .before (s =>
+    .up (s =>
     {
+        s.called = [];
+
         const Service = s.http.defineService ("TestService")
-            .onStop (function ()
-            {
-                this.stopArgs = nit.array (arguments);
-                this.stopCalled = true;
-            })
+            .onPreStop (() => s.called.push ("preStopService"))
+            .onPostStop (() => s.called.push ("postStopService"))
+            .onStop (() => s.called.push ("stopService"))
         ;
 
-        s.object.services.push (new Service ());
+        s.class = s.class.defineSubclass ("MyHost")
+            .onPreStop (() => s.called.push ("preStopHost"))
+            .onPostStop (() => s.called.push ("postStopHost"))
+            .onStop (() => s.called.push ("stopHost"))
+        ;
+
+        s.createArgs = { services: new Service () };
     })
-    .given (1, 2, 3)
-    .returns ()
-    .expectingPropertyToBe ("object.services.0.stopCalled", true)
-    .expectingPropertyToBe ("object.services.0.stopArgs", [])
+    .before (s => s.object.preStop ())
+    .after (s => s.object.postStop ())
+    .returnsResultOfExpr ("object")
+    .expectingPropertyToBe ("called", ["preStopService", "preStopHost", "stopService", "stopHost", "postStopService", "postStopHost"])
     .commit ()
 ;
 
 
 test.method ("http.Host", "dispatch")
     .should ("invoke services' dispatch method")
-    .before (s =>
+    .up (s =>
     {
+        s.called = [];
+
         const Service = s.http.defineService ("TestService")
-            .onDispatch (function ()
-            {
-                this.dispatchArgs = nit.array (arguments);
-                this.dispatchCalled = true;
-            })
+            .onPreDispatch (() => s.called.push ("preDispatchService"))
+            .onPostDispatch (() => s.called.push ("postDispatchService"))
+            .onDispatch (() => s.called.push ("dispatchService"))
         ;
 
-        s.object.services.push (new Service ());
+        s.class = s.class.defineSubclass ("MyHost")
+            .onPreDispatch (() => s.called.push ("preDispatchHost"))
+            .onPostDispatch (() => s.called.push ("postDispatchHost"))
+            .onDispatch (() => s.called.push ("dispatchHost"))
+        ;
+
+        s.createArgs =
+        {
+            services: new Service ()
+        };
+
+        s.args = s.Context.new ();
     })
-    .given (nit.require ("http.Context").new ("GET", "/"))
-    .returns ()
-    .expectingPropertyToBe ("object.services.0.dispatchCalled", true)
-    .expectingPropertyToBe ("object.services.0.dispatchArgs.length", 1)
-    .expectingPropertyToBeOfType ("object.services.0.dispatchArgs.0", "http.Context")
+    .returnsResultOfExpr ("args.0")
+    .expectingPropertyToBe ("called", ["preDispatchHost", "dispatchHost", "preDispatchService", "dispatchService", "postDispatchService", "postDispatchHost"])
+    .expectingPropertyToBeOfType ("args.0.service", "http.services.TestService")
+    .commit ()
+;
+
+
+test.method ("http.Host", "upgrade")
+    .should ("invoke target service's upgrade method")
+    .up (s =>
+    {
+        s.called = [];
+
+        const Service = s.http.defineService ("TestService")
+            .onPreUpgrade (() => s.called.push ("preUpgradeService"))
+            .onPostUpgrade (() => s.called.push ("postUpgradeService"))
+            .onUpgrade (() => s.called.push ("upgradeService"))
+            .onUpgrade (req => s.upgradeArg = req)
+        ;
+
+        s.class = s.class.defineSubclass ("MyHost")
+            .onPreUpgrade (() => s.called.push ("preUpgradeHost"))
+            .onPostUpgrade (() => s.called.push ("postUpgradeHost"))
+            .onUpgrade (() => s.called.push ("upgradeHost"))
+        ;
+
+        s.createArgs =
+        {
+            services: new Service ()
+        };
+
+        s.args = [
+            new s.IncomingMessage ("GET", "/", { headers: { host: "app.pushcorn.com" } }),
+            new s.Socket
+        ];
+    })
+    .returnsResultOfExpr ("object")
+    .expectingPropertyToBe ("called", ["preUpgradeHost", "upgradeHost", "preUpgradeService", "upgradeService", "postUpgradeService", "postUpgradeHost"])
+    .expectingPropertyToBeOfType ("upgradeArg", "http.mocks.IncomingMessage")
     .commit ()
 ;
