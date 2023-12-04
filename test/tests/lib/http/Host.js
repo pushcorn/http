f8b790def83c5a5da88c02c1ac591d0eb9a9fd8b
Joseph Tzeng <joseph@pushcorn.com>
2023-12-04 14:37:32 -0500

    adjusts plugin invocation order

--- test/tests/lib/http/Host.js
+++ test/tests/lib/http/Host.js
@@ -45,151 +45,278 @@ test.object ("http.Host")
 
 test.method ("http.Host", "init")
     .should ("initialize the services")
-    .up (s =>
-    {
-        s.called = [];
+        .up (s =>
+        {
+            s.called = [];
 
-        const Service = s.http.defineService ("TestService")
-            .onPreInit (() => s.called.push ("preInitService"))
-            .onPostInit (() => s.called.push ("postInitService"))
-            .onInit (() => s.called.push ("initService"))
-        ;
+            const Service = s.http.defineService ("TestService")
+                .onPreInit (() => s.called.push ("preInitService"))
+                .onPostInit (() => s.called.push ("postInitService"))
+                .onInit (() => s.called.push ("initService"))
+            ;
 
-        s.class = s.class.defineSubclass ("MyHost")
-            .onPreInit (() => s.called.push ("preInitHost"))
-            .onPostInit (() => s.called.push ("postInitHost"))
-            .onInit (() => s.called.push ("initHost"))
-        ;
+            const MyPlugin = s.http.defineHostPlugin ("MyPlugin")
+                .onPreInit (() => s.called.push ("preInitPlugin"))
+                .onPostInit (() => s.called.push ("postInitPlugin"))
+                .onInit (() => s.called.push ("initPlugin"))
+            ;
 
-        s.createArgs = { services: new Service () };
-    })
-    .before (s => s.object.preInit ())
-    .after (s => s.object.postInit ())
-    .returnsResultOfExpr ("object")
-    .expectingPropertyToBe ("called", ["preInitHost", "preInitService", "initHost", "initService", "postInitHost", "postInitService"])
-    .commit ()
+            s.class = s.class.defineSubclass ("MyHost")
+                .onPreInit (() => s.called.push ("preInitHost"))
+                .onPostInit (() => s.called.push ("postInitHost"))
+                .onInit (() => s.called.push ("initHost"))
+            ;
+
+            s.class.hostplugin (new MyPlugin);
+            s.createArgs = { services: new Service () };
+        })
+        .before (s => s.object.preInit ())
+        .after (s => s.object.postInit ())
+        .returnsResultOfExpr ("object")
+        .expectingPropertyToBe ("called",
+        [
+            "preInitHost",
+            "preInitPlugin",
+            "preInitService",
+            "initHost",
+            "initPlugin",
+            "initService",
+            "postInitHost",
+            "postInitPlugin",
+            "postInitService"
+        ])
+        .commit ()
 ;
 
 
 test.method ("http.Host", "start")
     .should ("invoke services' start method")
-    .up (s =>
-    {
-        s.called = [];
+        .up (s =>
+        {
+            s.called = [];
 
-        const Service = s.http.defineService ("TestService")
-            .onPreStart (() => s.called.push ("preStartService"))
-            .onPostStart (() => s.called.push ("postStartService"))
-            .onStart (() => s.called.push ("startService"))
-        ;
+            const Service = s.http.defineService ("TestService")
+                .onPreStart (() => s.called.push ("preStartService"))
+                .onPostStart (() => s.called.push ("postStartService"))
+                .onStart (() => s.called.push ("startService"))
+            ;
 
-        s.class = s.class.defineSubclass ("MyHost")
-            .onPreStart (() => s.called.push ("preStartHost"))
-            .onPostStart (() => s.called.push ("postStartHost"))
-            .onStart (() => s.called.push ("startHost"))
-        ;
+            const MyPlugin = s.http.defineHostPlugin ("MyPlugin")
+                .onPreStart (() => s.called.push ("preStartPlugin"))
+                .onPostStart (() => s.called.push ("postStartPlugin"))
+                .onStart (() => s.called.push ("startPlugin"))
+            ;
 
-        s.createArgs = { services: new Service () };
-    })
-    .before (s => s.object.preStart ())
-    .after (s => s.object.postStart ())
-    .returnsResultOfExpr ("object")
-    .expectingPropertyToBe ("called", ["preStartHost", "preStartService", "startHost", "startService", "postStartHost", "postStartService"])
-    .commit ()
+            s.class = s.class.defineSubclass ("MyHost")
+                .onPreStart (() => s.called.push ("preStartHost"))
+                .onPostStart (() => s.called.push ("postStartHost"))
+                .onStart (() => s.called.push ("startHost"))
+            ;
+
+            s.class.hostplugin (new MyPlugin);
+            s.createArgs = { services: new Service () };
+        })
+        .before (s => s.object.preStart ())
+        .after (s => s.object.postStart ())
+        .returnsResultOfExpr ("object")
+        .expectingPropertyToBe ("called",
+        [
+            "preStartHost",
+            "preStartPlugin",
+            "preStartService",
+            "startHost",
+            "startPlugin",
+            "startService",
+            "postStartHost",
+            "postStartPlugin",
+            "postStartService"
+        ])
+        .commit ()
 ;
 
 
 test.method ("http.Host", "stop")
     .should ("invoke services' stop method")
-    .up (s =>
-    {
-        s.called = [];
+        .up (s =>
+        {
+            s.called = [];
 
-        const Service = s.http.defineService ("TestService")
-            .onPreStop (() => s.called.push ("preStopService"))
-            .onPostStop (() => s.called.push ("postStopService"))
-            .onStop (() => s.called.push ("stopService"))
-        ;
+            const Service = s.http.defineService ("TestService")
+                .onPreStop (() => s.called.push ("preStopService"))
+                .onPostStop (() => s.called.push ("postStopService"))
+                .onStop (() => s.called.push ("stopService"))
+            ;
 
-        s.class = s.class.defineSubclass ("MyHost")
-            .onPreStop (() => s.called.push ("preStopHost"))
-            .onPostStop (() => s.called.push ("postStopHost"))
-            .onStop (() => s.called.push ("stopHost"))
-        ;
+            const MyPlugin = s.http.defineHostPlugin ("MyPlugin")
+                .onPreStop (() => s.called.push ("preStopPlugin"))
+                .onPostStop (() => s.called.push ("postStopPlugin"))
+                .onStop (() => s.called.push ("stopPlugin"))
+            ;
 
-        s.createArgs = { services: new Service () };
-    })
-    .before (s => s.object.preStop ())
-    .after (s => s.object.postStop ())
-    .returnsResultOfExpr ("object")
-    .expectingPropertyToBe ("called", ["preStopService", "preStopHost", "stopService", "stopHost", "postStopService", "postStopHost"])
-    .commit ()
+            s.class = s.class.defineSubclass ("MyHost")
+                .onPreStop (() => s.called.push ("preStopHost"))
+                .onPostStop (() => s.called.push ("postStopHost"))
+                .onStop (() => s.called.push ("stopHost"))
+            ;
+
+            s.class.hostplugin (new MyPlugin);
+            s.createArgs = { services: new Service () };
+        })
+        .before (s => s.object.preStop ())
+        .after (s => s.object.postStop ())
+        .returnsResultOfExpr ("object")
+        .expectingPropertyToBe ("called",
+        [
+            "preStopService",
+            "preStopHost",
+            "preStopPlugin",
+            "stopService",
+            "stopHost",
+            "stopPlugin",
+            "postStopService",
+            "postStopHost",
+            "postStopPlugin"
+        ])
+        .commit ()
 ;
 
 
 test.method ("http.Host", "dispatch")
-    .should ("invoke services' dispatch method")
-    .up (s =>
-    {
-        s.called = [];
-
-        const Service = s.http.defineService ("TestService")
-            .onPreDispatch (() => s.called.push ("preDispatchService"))
-            .onPostDispatch (() => s.called.push ("postDispatchService"))
-            .onDispatch (() => s.called.push ("dispatchService"))
-        ;
-
-        s.class = s.class.defineSubclass ("MyHost")
-            .onPreDispatch (() => s.called.push ("preDispatchHost"))
-            .onPostDispatch (() => s.called.push ("postDispatchHost"))
-            .onDispatch (() => s.called.push ("dispatchHost"))
-        ;
-
-        s.createArgs =
+    .should ("invoke the target service's dispatch method")
+        .up (s =>
         {
-            services: new Service ()
-        };
+            s.called = [];
 
-        s.args = s.Context.new ();
-    })
-    .returnsResultOfExpr ("args.0")
-    .expectingPropertyToBe ("called", ["preDispatchHost", "dispatchHost", "preDispatchService", "dispatchService", "postDispatchService", "postDispatchHost"])
-    .expectingPropertyToBeOfType ("args.0.service", "http.services.TestService")
-    .commit ()
+            const Service = s.http.defineService ("TestService")
+                .onPreDispatch (() => s.called.push ("preDispatchService"))
+                .onPostDispatch (() => s.called.push ("postDispatchService"))
+                .onDispatch (() => s.called.push ("dispatchService"))
+            ;
+
+            const MyPlugin = s.http.defineHostPlugin ("MyPlugin")
+                .onPreDispatch (() => s.called.push ("preDispatchPlugin"))
+                .onPostDispatch (() => s.called.push ("postDispatchPlugin"))
+                .onDispatch (() => s.called.push ("dispatchPlugin"))
+            ;
+
+            s.class = s.class.defineSubclass ("MyHost")
+                .onPreDispatch (() => s.called.push ("preDispatchHost"))
+                .onPostDispatch (() => s.called.push ("postDispatchHost"))
+                .onDispatch (() => s.called.push ("dispatchHost"))
+            ;
+
+            s.class.hostplugin (new MyPlugin);
+            s.createArgs = { services: new Service () };
+            s.args = s.Context.new ();
+        })
+        .returnsResultOfExpr ("args.0")
+        .expectingPropertyToBeOfType ("args.0.service", "http.services.TestService")
+        .expectingPropertyToBe ("called",
+        [
+            "preDispatchHost",
+            "preDispatchPlugin",
+            "dispatchHost",
+            "dispatchPlugin",
+            "preDispatchService",
+            "dispatchService",
+            "postDispatchService",
+            "postDispatchHost",
+            "postDispatchPlugin"
+        ])
+        .commit ()
+
+    .should ("NOT call the service if the plugin set the response")
+        .up (s =>
+        {
+            s.called = [];
+
+            const Service = s.http.defineService ("TestService")
+                .onPreDispatch (() => s.called.push ("preDispatchService"))
+                .onPostDispatch (() => s.called.push ("postDispatchService"))
+                .onDispatch (() => s.called.push ("dispatchService"))
+            ;
+
+            const MyPlugin = s.http.defineHostPlugin ("MyPlugin")
+                .onPreDispatch (() => s.called.push ("preDispatchPlugin"))
+                .onPostDispatch (() => s.called.push ("postDispatchPlugin"))
+                .onDispatch ((service, ctx) =>
+                {
+                    ctx.noop ();
+                    s.called.push ("dispatchPlugin");
+                })
+            ;
+
+            s.class = s.class.defineSubclass ("MyHost")
+                .onPreDispatch (() => s.called.push ("preDispatchHost"))
+                .onPostDispatch (() => s.called.push ("postDispatchHost"))
+                .onDispatch (() => s.called.push ("dispatchHost"))
+            ;
+
+            s.class.hostplugin (new MyPlugin);
+            s.createArgs = { services: new Service () };
+            s.args = s.Context.new ();
+        })
+        .returnsResultOfExpr ("args.0")
+        .expectingPropertyToBeOfType ("args.0.service", "http.services.TestService")
+        .expectingPropertyToBe ("called",
+        [
+            "preDispatchHost",
+            "preDispatchPlugin",
+            "dispatchHost",
+            "dispatchPlugin",
+            "postDispatchHost",
+            "postDispatchPlugin"
+        ])
+        .commit ()
 ;
 
 
 test.method ("http.Host", "upgrade")
     .should ("invoke target service's upgrade method")
-    .up (s =>
-    {
-        s.called = [];
-
-        const Service = s.http.defineService ("TestService")
-            .onPreUpgrade (() => s.called.push ("preUpgradeService"))
-            .onPostUpgrade (() => s.called.push ("postUpgradeService"))
-            .onUpgrade (() => s.called.push ("upgradeService"))
-            .onUpgrade (req => s.upgradeArg = req)
-        ;
-
-        s.class = s.class.defineSubclass ("MyHost")
-            .onPreUpgrade (() => s.called.push ("preUpgradeHost"))
-            .onPostUpgrade (() => s.called.push ("postUpgradeHost"))
-            .onUpgrade (() => s.called.push ("upgradeHost"))
-        ;
-
-        s.createArgs =
+        .up (s =>
         {
-            services: new Service ()
-        };
+            s.called = [];
 
-        s.args = [
-            new s.IncomingMessage ("GET", "/", { headers: { host: "app.pushcorn.com" } }),
-            new s.Socket
-        ];
-    })
-    .returnsResultOfExpr ("object")
-    .expectingPropertyToBe ("called", ["preUpgradeHost", "upgradeHost", "preUpgradeService", "upgradeService", "postUpgradeService", "postUpgradeHost"])
-    .expectingPropertyToBeOfType ("upgradeArg", "http.mocks.IncomingMessage")
-    .commit ()
+            const Service = s.http.defineService ("TestService")
+                .onPreUpgrade (() => s.called.push ("preUpgradeService"))
+                .onPostUpgrade (() => s.called.push ("postUpgradeService"))
+                .onUpgrade (() => s.called.push ("upgradeService"))
+                .onUpgrade (req => s.upgradeArg = req)
+            ;
+
+            const MyPlugin = s.http.defineHostPlugin ("MyPlugin")
+                .onPreUpgrade (() => s.called.push ("preUpgradePlugin"))
+                .onPostUpgrade (() => s.called.push ("postUpgradePlugin"))
+                .onUpgrade (() => s.called.push ("upgradePlugin"))
+            ;
+
+            s.class = s.class.defineSubclass ("MyHost")
+                .onPreUpgrade (() => s.called.push ("preUpgradeHost"))
+                .onPostUpgrade (() => s.called.push ("postUpgradeHost"))
+                .onUpgrade (() => s.called.push ("upgradeHost"))
+            ;
+
+            s.class.hostplugin (new MyPlugin);
+            s.createArgs = { services: new Service () };
+            s.args =
+            [
+                new s.IncomingMessage ("GET", "/", { headers: { host: "app.pushcorn.com" } }),
+                new s.Socket
+            ];
+        })
+        .returnsResultOfExpr ("object")
+        .expectingPropertyToBe ("called",
+        [
+            "preUpgradeHost",
+            "preUpgradePlugin",
+            "upgradeHost",
+            "upgradePlugin",
+            "preUpgradeService",
+            "upgradeService",
+            "postUpgradeService",
+            "postUpgradeHost",
+            "postUpgradePlugin"
+        ])
+        .expectingPropertyToBeOfType ("upgradeArg", "http.mocks.IncomingMessage")
+        .commit ()
 ;
--- test/tests/lib/http/Service.js
+++ test/tests/lib/http/Service.js
@@ -29,38 +29,94 @@ test.method ("http.Service", "dispatch")
 ;
 
 
+test.method ("http.Service", "init")
+    .should ("initialize the service")
+        .up (s =>
+        {
+            s.called = [];
+
+            const MyPlugin = s.http.defineServicePlugin ("MyPlugin")
+                .onPreInit (() => s.called.push ("preInitPlugin"))
+                .onPostInit (() => s.called.push ("postInitPlugin"))
+                .onInit (() => s.called.push ("initPlugin"))
+            ;
+
+            const MyHandler = s.http.defineHandler ("MyHandler")
+                .onPreInit (() => s.called.push ("preInitHandler"))
+                .onPostInit (() => s.called.push ("postInitHandler"))
+                .onInit (() => s.called.push ("initHandler"))
+            ;
+
+            s.class = s.class.defineSubclass ("MyService")
+                .onPreInit (() => s.called.push ("preInitService"))
+                .onPostInit (() => s.called.push ("postInitService"))
+                .onInit (() => s.called.push ("initService"))
+            ;
+
+            s.class.serviceplugin (new MyPlugin);
+            s.createArgs = { handlers: new MyHandler };
+        })
+        .before (s => s.object.preInit ())
+        .after (s => s.object.postInit ())
+        .returnsResultOfExpr ("object")
+        .expectingPropertyToBe ("called",
+        [
+            "preInitService",
+            "preInitPlugin",
+            "preInitHandler",
+            "initService",
+            "initPlugin",
+            "initHandler",
+            "postInitService",
+            "postInitPlugin",
+            "postInitHandler"
+        ])
+        .commit ()
+;
+
+
 test.method ("http.Service", "start")
     .should ("start the service")
         .up (s =>
         {
-            const Plugin = s.http.defineServicePlugin ("MyPlugin", true)
-                .onPreStart (() => s.preStart = true)
-                .onPostStart (() => s.postStart = true)
+            s.called = [];
+
+            const MyPlugin = s.http.defineServicePlugin ("MyPlugin")
+                .onPreStart (() => s.called.push ("preStartPlugin"))
+                .onPostStart (() => s.called.push ("postStartPlugin"))
+                .onStart (() => s.called.push ("startPlugin"))
             ;
 
-            const MyApi = s.http.defineApi ("MyApi")
-                .condition ("http:request-path", "/one")
-                .onPreStart (() => s.handlerPreStart = true)
-                .onPostStart (() => s.handlerPostStart = true)
-                .onStart (() => s.handlerStart = true)
+            const MyHandler = s.http.defineHandler ("MyHandler")
+                .onPreStart (() => s.called.push ("preStartHandler"))
+                .onPostStart (() => s.called.push ("postStartHandler"))
+                .onStart (() => s.called.push ("startHandler"))
             ;
 
             s.class = s.class.defineSubclass ("MyService")
-                .serviceplugin (new Plugin)
-                .onStart (() => s.startCalled = true)
+                .onPreStart (() => s.called.push ("preStartService"))
+                .onPostStart (() => s.called.push ("postStartService"))
+                .onStart (() => s.called.push ("startService"))
             ;
 
-            s.createArgs = { handlers: new MyApi };
+            s.class.serviceplugin (new MyPlugin);
+            s.createArgs = { handlers: new MyHandler };
         })
         .before (s => s.object.preStart ())
-        .before (s => s.object.postStart ())
+        .after (s => s.object.postStart ())
         .returnsResultOfExpr ("object")
-        .expectingPropertyToBe ("preStart", true)
-        .expectingPropertyToBe ("postStart", true)
-        .expectingPropertyToBe ("startCalled", true)
-        .expectingPropertyToBe ("handlerPreStart", true)
-        .expectingPropertyToBe ("handlerPostStart", true)
-        .expectingPropertyToBe ("handlerStart", true)
+        .expectingPropertyToBe ("called",
+        [
+            "preStartService",
+            "preStartPlugin",
+            "preStartHandler",
+            "startService",
+            "startPlugin",
+            "startHandler",
+            "postStartService",
+            "postStartPlugin",
+            "postStartHandler"
+        ])
         .commit ()
 ;
 
@@ -69,34 +125,44 @@ test.method ("http.Service", "stop")
     .should ("stop the service")
         .up (s =>
         {
-            const Plugin = s.http.defineServicePlugin ("MyPlugin", true)
-                .onPreStop (() => s.preStop = true)
-                .onPostStop (() => s.postStop = true)
+            s.called = [];
+
+            const MyPlugin = s.http.defineServicePlugin ("MyPlugin")
+                .onPreStop (() => s.called.push ("preStopPlugin"))
+                .onPostStop (() => s.called.push ("postStopPlugin"))
+                .onStop (() => s.called.push ("stopPlugin"))
             ;
 
-            const MyApi = s.http.defineApi ("MyApi")
-                .condition ("http:request-path", "/one")
-                .onPreStop (() => s.handlerPreStop = true)
-                .onPostStop (() => s.handlerPostStop = true)
-                .onStop (() => s.handlerStop = true)
+            const MyHandler = s.http.defineHandler ("MyHandler")
+                .onPreStop (() => s.called.push ("preStopHandler"))
+                .onPostStop (() => s.called.push ("postStopHandler"))
+                .onStop (() => s.called.push ("stopHandler"))
             ;
 
             s.class = s.class.defineSubclass ("MyService")
-                .serviceplugin (new Plugin)
-                .onStop (() => s.stopCalled = true)
+                .onPreStop (() => s.called.push ("preStopService"))
+                .onPostStop (() => s.called.push ("postStopService"))
+                .onStop (() => s.called.push ("stopService"))
             ;
 
-            s.createArgs = { apis: new MyApi };
+            s.class.serviceplugin (new MyPlugin);
+            s.createArgs = { handlers: new MyHandler };
         })
         .before (s => s.object.preStop ())
-        .before (s => s.object.postStop ())
+        .after (s => s.object.postStop ())
         .returnsResultOfExpr ("object")
-        .expectingPropertyToBe ("preStop", true)
-        .expectingPropertyToBe ("postStop", true)
-        .expectingPropertyToBe ("stopCalled", true)
-        .expectingPropertyToBe ("handlerPreStop", true)
-        .expectingPropertyToBe ("handlerPostStop", true)
-        .expectingPropertyToBe ("handlerStop", true)
+        .expectingPropertyToBe ("called",
+        [
+            "preStopHandler",
+            "preStopService",
+            "preStopPlugin",
+            "stopHandler",
+            "stopService",
+            "stopPlugin",
+            "postStopHandler",
+            "postStopService",
+            "postStopPlugin"
+        ])
         .commit ()
 ;
 
@@ -105,25 +171,82 @@ test.method ("http.Service", "dispatch")
     .should ("skip the dispatch callback if the response is set by a plugin")
         .up (s =>
         {
-            const Plugin = s.http.defineServicePlugin ("MyPlugin")
+            s.called = [];
+
+            const MyPlugin = s.http.defineServicePlugin ("MyPlugin")
                 .onPreDispatch ((service, ctx) =>
                 {
-                    ctx.send ("http:noop");
+                    ctx.noop ();
 
-                    s.preDispatch = true;
+                    s.called.push ("preDispatchPlugin");
                 })
-                .onPostDispatch (() => s.postDispatch = true)
+                .onPostDispatch (() => s.called.push ("postDispatchPlugin"))
+                .onDispatch (() => s.called.push ("dispatchPlugin"))
             ;
 
-            s.class = s.class.defineSubclass ("MyService");
-            s.class.serviceplugin (new Plugin);
-            s.class.onDispatch (() => s.dispatchCalled = true);
+            const MyHandler = s.http.defineHandler ("MyHandler")
+                .onPreRun (() => s.called.push ("preRunHandler"))
+                .onPostRun (() => s.called.push ("postRunHandler"))
+                .onRun (() => s.called.push ("runHandler"))
+            ;
+
+            s.class = s.class.defineSubclass ("MyService")
+                .onPreDispatch (() => s.called.push ("preDispatchService"))
+                .onPostDispatch (() => s.called.push ("postDispatchService"))
+                .onDispatch (() => s.called.push ("dispatchService"))
+            ;
+
+            s.class.serviceplugin (new MyPlugin);
+            s.createArgs = { handlers: new MyHandler };
         })
         .given (Context.new ())
-        .returnsResultOfExpr ("object")
-        .expectingPropertyToBe ("preDispatch", true)
-        .expectingPropertyToBe ("postDispatch", true)
-        .expectingPropertyToBe ("dispatchCalled", undefined)
+        .returnsResultOfExpr ("args.0")
+        .expectingPropertyToBe ("called", ["preDispatchService", "preDispatchPlugin", "postDispatchService", "postDispatchPlugin"])
+        .commit ()
+
+    .should ("run the handler if the response was not set by the plugin")
+        .up (s =>
+        {
+            s.called = [];
+
+            const MyPlugin = s.http.defineServicePlugin ("MyPlugin")
+                .onPreDispatch (() => s.called.push ("preDispatchPlugin"))
+                .onPostDispatch (() => s.called.push ("postDispatchPlugin"))
+                .onDispatch (() => s.called.push ("dispatchPlugin"))
+            ;
+
+            const MyHandler = s.http.defineHandler ("MyHandler")
+                .onPreRun (() => s.called.push ("preRunHandler"))
+                .onPostRun (() => s.called.push ("postRunHandler"))
+                .onRun (ctx =>
+                {
+                    ctx.noop ();
+
+                    s.called.push ("runHandler");
+                })
+            ;
+
+            s.class = s.class.defineSubclass ("MyService")
+                .onPreDispatch (() => s.called.push ("preDispatchService"))
+                .onPostDispatch (() => s.called.push ("postDispatchService"))
+                .onDispatch (() => s.called.push ("dispatchService"))
+            ;
+
+            s.class.serviceplugin (new MyPlugin);
+            s.createArgs = { handlers: new MyHandler };
+        })
+        .given (Context.new ())
+        .returnsResultOfExpr ("args.0")
+        .expectingPropertyToBe ("called",
+        [
+            "preDispatchService",
+            "preDispatchPlugin",
+            "dispatchService",
+            "preRunHandler",
+            "runHandler",
+            "postDispatchService",
+            "postDispatchPlugin"
+        ])
         .commit ()
 
     .should ("read the request if the hook was implemented")
@@ -133,9 +256,55 @@ test.method ("http.Service", "dispatch")
             s.class.onDispatch (() => s.dispatchCalled = true);
         })
         .given (Context.new ())
-        .returnsResultOfExpr ("object")
+        .returnsResultOfExpr ("args.0")
         .expectingPropertyToBe ("dispatchCalled", true)
         .expectingPropertyToBe ("args.0.requestRead", true)
+        .commit ()
+;
+
+
+test.method ("http.Service", "upgrade")
+    .should ("upgrade the connection")
+        .up (s =>
+        {
+            s.called = [];
+
+            const MyPlugin = s.http.defineServicePlugin ("MyPlugin")
+                .onPreUpgrade (() => s.called.push ("preUpgradePlugin"))
+                .onPostUpgrade (() => s.called.push ("postUpgradePlugin"))
+                .onUpgrade (() => s.called.push ("upgradePlugin"))
+            ;
+
+            const MyHandler = s.http.defineHandler ("MyHandler")
+                .onPreRun (() => s.called.push ("preRunHandler"))
+                .onPostRun (() => s.called.push ("postRunHandler"))
+                .onRun (() => s.called.push ("runHandler"))
+            ;
+
+            s.class = s.class.defineSubclass ("MyService")
+                .onPreUpgrade (() => s.called.push ("preUpgradeService"))
+                .onPostUpgrade (() => s.called.push ("postUpgradeService"))
+                .onUpgrade (() => s.called.push ("upgradeService"))
+            ;
+
+            s.class.serviceplugin (new MyPlugin);
+            s.createArgs = { handlers: new MyHandler };
+            s.args =
+            [
+                new s.IncomingMessage ("GET", "/", { headers: { host: "app.pushcorn.com" } }),
+                new s.Socket
+            ];
+        })
+        .returnsResultOfExpr ("object")
+        .expectingPropertyToBe ("called",
+        [
+            "preUpgradeService",
+            "preUpgradePlugin",
+            "upgradeService",
+            "upgradePlugin",
+            "postUpgradeService",
+            "postUpgradePlugin"
+        ])
         .commit ()
 ;
 
