efa0ff0c3c4eaef6d75c66d0137a850316a670e3
Joseph Tzeng <joseph@pushcorn.com>
2023-12-05 12:53:15 -0500

    makes the response filter a plugin

--- test/tests/lib/http/RequestFilter.js
+++ test/tests/lib/http/RequestFilter.js
@@ -1,45 +1,35 @@
-const { Readable } = require ("stream");
-
-
-test.method ("http.RequestFilter.Descriptor", "build", true)
-    .should ("return an instance of http.RequestFilter")
-        .given ("http:json-body-parser")
-        .returnsInstanceOf ("http.requestfilters.JsonBodyParser")
+test.plugin ("http.RequestFilter", "filterRequest", { instancePluginAllowed: true, registerPlugin: true })
+    .should ("filter the request")
+        .up (s => s.hostArgs = { requestfilters: "http:json-body-parser" })
+        .up (s => s.args = s.Context.new ({ headers: { "content-type": "application/json" } }))
+        .mock ("http.requestfilters.JsonBodyParser.prototype", "apply")
+        .expectingPropertyToBe ("mocks.0.invocations.length", 1)
         .commit ()
 
-    .should ("adds the specified conditions to the filter class")
-        .given ("http:json-body-parser",
-        {
-            conditions:
-            {
-                name: "http:request-method",
-                options: "POST"
-            }
-        })
-        .returnsInstanceOf ("http.requestfilters.JsonBodyParser")
-        .expectingPropertyToBe ("result.constructor.conditions.length", 1)
-        .expectingPropertyToBeOfType ("result.constructor.conditions.0", "http.conditions.RequestMethod")
+    .should ("skip the filters no applicable to the context")
+        .up (s => s.hostArgs = { requestfilters: "http:json-body-parser" })
+        .up (s => s.args = s.Context.new ())
         .commit ()
 ;
 
 
 test.method ("http.RequestFilter", "parseStream", true)
     .should ("parse the stream with the given encoding")
-        .given (Readable.from (Buffer.from ("test")))
+        .up (s => s.args = s.bufferToStream ("test"))
         .returns (Buffer.from ("test"))
         .commit ()
 
-    .given (Readable.from (Buffer.from ("test")), "utf8")
+    .up (s => s.args = [s.bufferToStream ("test"), "utf8"])
         .returns ("test")
         .commit ()
 
     .should ("throw the status code if the erro has the status property")
-        .given (Readable.from (Buffer.from ("test")), "unsupported")
+        .up (s => s.args = [s.bufferToStream ("test"), "unsupported"])
         .throws (415)
         .commit ()
 
     .should ("throw if the parsing failed")
-        .given (Readable.from (Buffer.from ("test")), "unsupported")
+        .up (s => s.args = [s.bufferToStream ("test"), "unsupported"])
         .mock (require ("bytes"), "parse", function ()
         {
             throw new Error ("ERR!");
@@ -51,7 +41,7 @@ test.method ("http.RequestFilter", "parseStream", true)
 
 test.method ("http.RequestFilter", "readBodyAsBuffer", true)
     .should ("read the request stream as a buffer")
-    .given ({ requestStream: Readable.from (Buffer.from ("test")) })
+    .up (s => s.args = { requestStream: s.bufferToStream ("test") })
     .returns (Buffer.from ("test"))
     .commit ()
 ;
@@ -59,11 +49,11 @@ test.method ("http.RequestFilter", "readBodyAsBuffer", true)
 
 test.method ("http.RequestFilter", "readBodyAsString", true)
     .should ("read the request stream as a string")
-        .given ({ requestStream: Readable.from (Buffer.from ("test")) })
+    .up (s => s.args = { requestStream: s.bufferToStream ("test") })
         .returns ("test")
         .commit ()
 
-    .given ({ requestStream: Readable.from (Buffer.from ("test")) }, "base64")
+    .up (s => s.args = [{ requestStream: s.bufferToStream ("test") }, "base64"])
         .returns ("dGVzdA==")
         .commit ()
 ;
--- test/tests/lib/http/ResponseFilter.js
+++ test/tests/lib/http/ResponseFilter.js
@@ -1,18 +1,36 @@
-const { Readable } = require ("stream");
-
-
-test.method ("http.ResponseFilter.Descriptor", "build", true)
-    .should ("return an instance of ResponseFilter")
-        .given ("http:cache-controller",
-        {
-            conditions:
+test.plugin ("http.ResponseFilter", "filterResponse", { instancePluginAllowed: true, registerPlugin: true })
+    .should ("filter the response")
+        .up (s => s.http.defineResponseFilter ("test.responsefilters.MyFilter")
+            .condition ("http:request-content-type", "application/json")
+            .onApply (ctx =>
             {
-                name: "http:hostname",
-                options: "app.pushcorn.com"
-            }
-        })
-        .returnsInstanceOf ("http.responsefilters.CacheController")
-        .expectingPropertyToBe ("result.constructor.conditions.length", 1)
+                ctx.response = { "@name": "http:json", json: { a: 3 } };
+                s.applied = true;
+            })
+        )
+        .up (s => s.http.defineResponseFilter ("test.responsefilters.AddHeader")
+            .condition ("http:request-content-type", "application/json")
+            .onApply (ctx =>
+            {
+                ctx.responseHeader ("my-header", "yes");
+            })
+        )
+        .up (s => s.hostArgs = { responsefilters: ["test:my-filter", "test:add-header"] })
+        .up (s => s.args = s.Context.new ({ headers: { "content-type": "application/json" } }, { status: 200 }))
+        .expectingPropertyToBe ("applied", true)
+        .expectingPropertyToBe ("args.0.responseHeaders.Content-Length", 7)
+        .expectingPropertyToBe ("args.0.responseHeaders.my-header", "yes")
+        .commit ()
+
+    .should ("skip the filters no applicable to the context")
+        .up (s => s.applied = false)
+        .up (s => s.http.defineResponseFilter ("test.responsefilters.MyFilter")
+            .condition ("http:request-content-type", "application/xml")
+            .onApply (() => s.applied = true)
+        )
+        .up (s => s.hostArgs = { responsefilters: "test:my-filter" })
+        .up (s => s.args = s.Context.new ({ headers: { "content-type": "application/json" } }, { status: 200 }))
+        .expectingPropertyToBe ("applied", false)
         .commit ()
 ;
 
@@ -24,7 +42,7 @@ test.method ("http.ResponseFilter", "readBodyAsString", true)
         .commit ()
 
     .should ("parse the response stream into a string")
-        .given ({ responseBody: Readable.from (Buffer.from ("test")) })
+        .up (s => s.args = { responseBody: s.bufferToStream ("test") })
         .returns ("test")
         .commit ()
 
